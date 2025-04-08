efca178fd3f1e2bd58f08b1bc97c478c17f9aa88
kpsmithjr <kpsmithjr@gmail.com>
2025-04-08 09:55:23 -0400

    Reverted method call for old library version

--- R/InternalFunctions.R
+++ R/InternalFunctions.R
@@ -23,7 +23,7 @@
 simplify.layers <- function(Input_Layer){
     
     ## Undirected Graphs
-    Layer <- as_undirected(Input_Layer, mode = c("collapse"),
+    Layer <- as.undirected(Input_Layer, mode = c("collapse"),
         edge.attr.comb = igraph_opt("edge.attr.comb"))
     
     ## Unweighted or Weigthed Graphs
